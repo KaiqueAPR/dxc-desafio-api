7db98609efd0efdd951b26bc94728b8ce1a89158
Kaique <kaiquepinho2010@hotmail.com>
2023-09-13 16:25:55 -0300

    Create project

---
+++ src/main/java/com/kaiqueapr/dxcdesafioapi/DxcDesafioApiApplication.java
@@ -0,0 +1,14 @@
+package com.kaiqueapr.dxcdesafioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxcDesafioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxcDesafioApiApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/kaiqueapr/dxcdesafioapi/DxcDesafioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiqueapr.dxcdesafioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxcDesafioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
