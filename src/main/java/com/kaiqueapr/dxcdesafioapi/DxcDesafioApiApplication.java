478117d5c7f9fab46c26c09aa7aa4752bde7fb6e
Kaique <kaiquepinho2010@hotmail.com>
2023-09-14 14:17:27 -0300

    fix: indentation

--- src/main/java/com/kaiqueapr/dxcdesafioapi/DxcDesafioApiApplication.java
+++ src/main/java/com/kaiqueapr/dxcdesafioapi/DxcDesafioApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DxcDesafioApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DxcDesafioApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DxcDesafioApiApplication.class, args);
+    }
 
 }
--- src/main/java/com/kaiqueapr/dxcdesafioapi/services/ClienteService.java
+++ src/main/java/com/kaiqueapr/dxcdesafioapi/services/ClienteService.java
@@ -19,63 +19,63 @@ import java.util.stream.Collectors;
 @Service
 public class ClienteService {
 
-@Autowired
-private ClienteRepository clienteRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
-/*Método responsável por criar um novo Cliente*/
-public ClienteResponseDto novoCliente(CriarClienteRequestDto clienteRequest) {
-ClienteModel cliente = new ClienteModel();
-BeanUtils.copyProperties(clienteRequest, cliente);
-cliente = clienteRepository.save(cliente);
-return converteParaDto(cliente);
-}
+    /*Método responsável por criar um novo Cliente*/
+    public ClienteResponseDto novoCliente(CriarClienteRequestDto clienteRequest) {
+        ClienteModel cliente = new ClienteModel();
+        BeanUtils.copyProperties(clienteRequest, cliente);
+        cliente = clienteRepository.save(cliente);
+        return converteParaDto(cliente);
+    }
 
-/*Método responsável por retornar uma lista de todos os Clientes*/
-public Page<ClienteResponseDto> listaClientes(Pageable pageable){
-Page<ClienteModel> all = clienteRepository.findAll(pageable);
-ModelMapper modelMapper = new ModelMapper();
-return new PageImpl<ClienteResponseDto>(all.stream().map(clienteModel -> modelMapper.map(clienteModel, ClienteResponseDto.class)).collect(Collectors.toList()));
+    /*Método responsável por retornar uma lista de todos os Clientes*/
+    public Page<ClienteResponseDto> listaClientes(Pageable pageable) {
+        Page<ClienteModel> all = clienteRepository.findAll(pageable);
+        ModelMapper modelMapper = new ModelMapper();
+        return new PageImpl<ClienteResponseDto>(all.stream().map(clienteModel -> modelMapper.map(clienteModel, ClienteResponseDto.class)).collect(Collectors.toList()));
 
-}
+    }
 
-/*Método responsável por achar um Cliente atráves da variável 'cdCliente'*/
-public ClienteResponseDto acharClientePorId(Integer cdCliente){
-Optional<ClienteModel> optional = clienteRepository.findById(cdCliente);
+    /*Método responsável por achar um Cliente atráves da variável 'cdCliente'*/
+    public ClienteResponseDto acharClientePorId(Integer cdCliente) {
+        Optional<ClienteModel> optional = clienteRepository.findById(cdCliente);
 
-if(optional.isEmpty()){
-throw new UserNotFound("O usuário que você tentou localizar não existe.");
-}
-return converteParaDto(optional.get());
-}
+        if (optional.isEmpty()) {
+            throw new UserNotFound("O usuário que você tentou localizar não existe.");
+        }
+        return converteParaDto(optional.get());
+    }
 
-/*Método responsável por deleter um Cliente atráves da variável 'cdCliente'*/
-public void deletarCliente(Integer cdCliente){
-if(clienteRepository.existsById(cdCliente)){
-clienteRepository.deleteById(cdCliente);
-}
-throw new UserNotFound("O usuário que você tentou localizar não existe.");
-}
+    /*Método responsável por deleter um Cliente atráves da variável 'cdCliente'*/
+    public void deletarCliente(Integer cdCliente) {
+        if (clienteRepository.existsById(cdCliente)) {
+            clienteRepository.deleteById(cdCliente);
+        }
+        throw new UserNotFound("O usuário que você tentou localizar não existe.");
+    }
 
-/*Método responsável por realizar um update no Cliente atráves da variável 'cdCliente'*/
-public ClienteResponseDto updatePorId(CriarClienteRequestDto criarClienteRequestDto, Integer cdCliente){
-Optional<ClienteModel> optional = clienteRepository.findById(cdCliente);
-if(optional.isPresent()){
-ClienteModel cliente = optional.get();
-BeanUtils.copyProperties(criarClienteRequestDto, cliente);
-cliente = clienteRepository.save(cliente);
-return converteParaDto(cliente);
-}
-throw new UserNotFound("O usuário que você tentou localizar não existe.");
-}
+    /*Método responsável por realizar um update no Cliente atráves da variável 'cdCliente'*/
+    public ClienteResponseDto updatePorId(CriarClienteRequestDto criarClienteRequestDto, Integer cdCliente) {
+        Optional<ClienteModel> optional = clienteRepository.findById(cdCliente);
+        if (optional.isPresent()) {
+            ClienteModel cliente = optional.get();
+            BeanUtils.copyProperties(criarClienteRequestDto, cliente);
+            cliente = clienteRepository.save(cliente);
+            return converteParaDto(cliente);
+        }
+        throw new UserNotFound("O usuário que você tentou localizar não existe.");
+    }
 
-/*Método responsável por converter um objeto para DTO*/
-private ClienteResponseDto converteParaDto(ClienteModel cliente) {
-ClienteResponseDto clienteDto = new ClienteResponseDto();
-clienteDto.setCdCliente(cliente.getCdCliente());
-clienteDto.setNmCliente(cliente.getNmCliente());
-clienteDto.setNmEmail(cliente.getNmEmail());
-clienteDto.setNrTelefone(cliente.getNrTelefone());
-return clienteDto;
-}
+    /*Método responsável por converter um objeto para DTO*/
+    private ClienteResponseDto converteParaDto(ClienteModel cliente) {
+        ClienteResponseDto clienteDto = new ClienteResponseDto();
+        clienteDto.setCdCliente(cliente.getCdCliente());
+        clienteDto.setNmCliente(cliente.getNmCliente());
+        clienteDto.setNmEmail(cliente.getNmEmail());
+        clienteDto.setNrTelefone(cliente.getNrTelefone());
+        return clienteDto;
+    }
 
 }
